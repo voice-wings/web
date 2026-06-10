a80a5e07e9a45c1d0ebd9c81d761690ef6da6889
muhammadanaswayanad <muhammadanaswayanad@gmail.com>
2026-06-10 13:07:26 +0530

    Optimize WebGL performance for mobile

--- src/components/WebGLBackground.jsx
+++ src/components/WebGLBackground.jsx
@@ -17,19 +17,19 @@ function AnimatedShapes() {
   return (
     <>
       <Float speed={2} rotationIntensity={1} floatIntensity={2} position={[-4, 2, -5]}>
-        <Sphere ref={sphereRef1} args={[2.5, 64, 64]}>
+        <Sphere ref={sphereRef1} args={[2.5, 32, 32]}>
           <MeshDistortMaterial color="#40247C" attach="material" distort={0.5} speed={2} roughness={0.2} metalness={0.8} />
         </Sphere>
       </Float>
       
       <Float speed={1.5} rotationIntensity={1.5} floatIntensity={2} position={[6, -1, -8]}>
-        <Sphere ref={sphereRef2} args={[3.5, 64, 64]}>
+        <Sphere ref={sphereRef2} args={[3.5, 32, 32]}>
           <MeshDistortMaterial color="#F281B3" attach="material" distort={0.4} speed={1.5} roughness={0.1} metalness={0.9} />
         </Sphere>
       </Float>
 
       <Float speed={2.5} rotationIntensity={0.5} floatIntensity={1} position={[-2, -6, -10]}>
-        <Sphere ref={sphereRef3} args={[4, 64, 64]}>
+        <Sphere ref={sphereRef3} args={[4, 32, 32]}>
           <MeshDistortMaterial color="#5DC1CD" attach="material" distort={0.3} speed={1} roughness={0.3} metalness={0.5} />
         </Sphere>
       </Float>
@@ -40,7 +40,11 @@ function AnimatedShapes() {
 export default function WebGLBackground() {
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: 0, pointerEvents: 'none' }}>
-      <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
+      <Canvas 
+        camera={{ position: [0, 0, 10], fov: 45 }}
+        dpr={[1, 1.5]}
+        performance={{ min: 0.5 }}
+      >
         <ambientLight intensity={0.6} />
         <directionalLight position={[10, 10, 5]} intensity={1.5} />
         <directionalLight position={[-10, -10, -5]} intensity={1} color="#F281B3" />
